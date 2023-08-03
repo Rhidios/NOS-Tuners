c23376a769f933342ec26d2627e2eb92930db621
Nicolas <nmpreiti@gmail.com>
2023-08-03 18:56:30 -0300

    all receipt commands now work properly and are ready for live

--- main.py
+++ main.py
@@ -4,91 +4,66 @@ import asyncio
 import datetime
 
 from discord.ext import commands
+import get_receipt_info
 
 
 intents = discord.Intents.all()
 
-TOKEN = "MTEzNjQ1ODMwNTk5NzM4OTk2NA.GxKL9O.klCyqJ9mSu5tRjhZ5hNIO8jFQ7e7aNwEYMYkJQ"
+TOKEN = "MTEzNjQ1ODMwNTk5NzM4OTk2NA.G7569J.i_zMnD2njqhoFeV150pY-xaARYl9uG5i8vNp9M"
 
 intents = discord.Intents().all()
 client = discord.Client(intents = intents)
 bot = commands.Bot(command_prefix='/', intents=intents)
 
-def get_next_activity_number():
-    with open('data.json', 'r') as file:
-        data = json.load(file)
-        activity_number = int(data['actividad_numero'])
-        receipts = data.get('recibos', [])
-    return activity_number, receipts
-
-def get_receipts_for_user(user_id):
-    activity_number, receipts = get_next_activity_number()
-    receipts_for_user = [receipt for receipt in receipts if receipt["UserID"] == user_id]
-    return receipts_for_user
-
-# def get_receipts_for_role(role_id):
-#     activity_number, receipts = get_next_activity_number()  # Unpack the tuple
-#     receipts_for_role = [receipt for receipt in receipts if receipt["RoleID"] == role_id]
-#     return receipts_for_role
-
-def update_activity_number(activity_number, receipts):
-    with open('data.json', 'w') as file:
-        data = {
-            "actividad_numero": activity_number,
-            "recibos": receipts
-        }
-        json.dump(data, file)
-
-def is_allowed_role(ctx):
-    allowed_roles = [1133497390247182396, 1133495683270332498, 1091845677455245352, 1091822086848266270, 1136467799900962836] #Last Id is for testing only
-    return any(role.id in allowed_roles for role in ctx.author.roles)
 
 @bot.command(name='mod_actividad')
-@commands.check(is_allowed_role)
+@commands.check(get_receipt_info.is_allowed_role)
 async def update_activity(ctx):
-    current_activity = get_next_activity_number()
+    current_activity = get_receipt_info.get_next_activity_number()
     await ctx.author.send(f"Cual es el numero de actividad correcto?: ")
     try:
         new_activity_number = await bot.wait_for('message', timeout=60.0, check=lambda message: message.author == ctx.author)
         new_activity_number = new_activity_number.content
     except asyncio.TimeoutError:
         return await ctx.author.send("Tiempo de espera agotado. Vuelve a intentarlo.")
-    update_activity_number(new_activity_number)
+    get_receipt_info.update_activity_number(new_activity_number)
 
     await ctx.send(f"Se ha cambiado el numero de actividad de {current_activity} a {new_activity_number}.")
 
-    # Delete the /mod_actividad command message
     await ctx.message.delete()
 
 @bot.command(name='ver_recibos')
-async def ver_recibos(ctx, user: discord.Member = None):
-    allowed_roles = [1133497390247182396, 1133495683270332498, 1091845677455245352, 1091822086848266270,1136467799900962836]
+async def ver_recibos(ctx, *users: discord.Member):
+    allowed_roles = [1133497390247182396, 1133495683270332498, 1091845677455245352, 1091822086848266270, 1136467799900962836]
     if not any(role.id in allowed_roles for role in ctx.author.roles):
-        return await ctx.send("No tienes permiso para usar este comando.")
+        return await ctx.author.send("No tienes permiso para usar este comando.")
 
-    if not user:
-        user = ctx.author
+    if not users:
+        users = [ctx.author]
 
-    user_id = user.id
-    # role_id = ctx.message.role_mentions[0].id if ctx.message.role_mentions else allowed_roles[0]
-    receipts_for_user = get_receipts_for_user(user_id)
+    for user in users:
+        user_id = user.id
+        receipts_for_user = get_receipt_info.get_receipts_for_user(user_id)
 
-    if not receipts_for_user:
-        return await ctx.author.send(f"No se encontraron recibos para el usuario con ID {user_id}.")
+        if not receipts_for_user:
+            await ctx.author.send(f"No se encontraron recibos para el usuario con ID {user_id}.")
+            continue
 
-    for receipt in receipts_for_user:
-        await ctx.author.send(f"Nombre del empleado: {receipt['Nombre']}")
-        await ctx.author.send(f"Fecha: {receipt['Dia']}")
-        await ctx.author.send(f"Hora: {receipt['Hora']}")
-        await ctx.author.send(f"Cajas restantes: {receipt['Cajas']}")
-        await ctx.author.send(f"Gasolina restante: {receipt['Gasolina']}")
-        await ctx.author.send(f"Actividad semanal: {receipt['Actividad Semanal']}")
-        await ctx.author.send(f"Comprobante: {receipt['Recibo']}")
-        await ctx.author.send("**━━━━━━━━━━━━━━━━━━━━━━━━━━━━━**")
+        await ctx.author.send(f"Recibos para el usuario {user.mention}:")
+        for receipt in receipts_for_user:
+            await ctx.author.send(f"Nombre del empleado: {receipt['Nombre']}")
+            await ctx.author.send(f"Fecha: {receipt['Dia']}")
+            await ctx.author.send(f"Hora: {receipt['Hora']}")
+            await ctx.author.send(f"Cajas restantes: {receipt['Cajas']}")
+            await ctx.author.send(f"Gasolina restante: {receipt['Gasolina']}")
+            await ctx.author.send(f"Actividad semanal: {receipt['Actividad Semanal']}")
+            await ctx.author.send(f"Comprobante: {receipt['Recibo']}")
+            await ctx.author.send("**━━━━━━━━━━━━━━━━━━━━━━━━━━━━━**")
 
-@bot.command(name = 'recibo')
+
+@bot.command(name='recibo')
 async def subir_recibo(ctx):
-    activity_number, receipts = get_next_activity_number()
+    activity_number, receipts = get_receipt_info.get_next_activity_number()
     user = ctx.author
     messages_to_delete = []
 
@@ -102,7 +77,6 @@ async def subir_recibo(ctx):
             await ctx.author.send("Tiempo de espera agotado. El intento se eliminará.")
             return True
 
-
     async def delete_messages():
         try:
             for message in messages_to_delete:
@@ -114,19 +88,18 @@ async def subir_recibo(ctx):
     if activity_number > 1:
         if await confirm_deletion():
             activity_number -= 1
-            update_activity_number(activity_number)
+            get_receipt_info.update_activity_number(activity_number)
             await ctx.author.send("El intento anterior ha sido eliminado. Vuelve a comenzar.")
             messages_to_delete.append(ctx.message)  # Delete the /recibo command message
             await delete_messages()
             return
 
-
     # Send the command message and add it to the list for deletion
-    messages_to_delete.append(await ctx.send("Comando /recibo"))
+    # messages_to_delete.append(await ctx.author.send("Comando /recibo"))
 
     await ctx.author.send(f"**Actividad N°: {activity_number}**")
     activity_number += 1
-    update_activity_number(activity_number,receipts)
+    get_receipt_info.update_activity_number(activity_number,receipts)
 
     await ctx.author.send("**Nombre del empleado (por favor mencionar con @): **")
     try:
@@ -194,7 +167,7 @@ async def subir_recibo(ctx):
             "recibos": receipts
         }
         json.dump(data, file)
-    get_next_activity_number()
+    get_receipt_info.get_next_activity_number()
 
 
     # Displaying all the information
@@ -209,12 +182,19 @@ async def subir_recibo(ctx):
     await ctx.send("**/ Comprobante:**")
     await ctx.send(receipt_image)
 
-    # Delete the command message after the command is done
-    await delete_messages()
+    # Delete the command message and prompt messages after the command is done
+    messages_to_delete.append(employee_name)
+    messages_to_delete.append(car_plate)
+    messages_to_delete.append(remaining_boxes)
+    messages_to_delete.append(remaining_fuel)
+    messages_to_delete.append(delivery_time)
+    messages_to_delete.append(weekly_activity)
+    messages_to_delete.append(receipt_image)
 
     try:
         await ctx.message.delete()
     except discord.NotFound:
         pass
+
 
 bot.run(TOKEN)
